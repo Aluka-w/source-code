ab7595e7bc1b43a46c43a437b90801a9f22f5f07
Aluka-w <1103500835@qq.com>
2020-04-29 15:21:15 +0800

    补充文档

--- JS原理/new.js
+++ JS原理/new.js
@@ -1,9 +1,10 @@
 /**
  * @description 实现 new
+ * ① 让实例可以访问到私有属性
+ * ② 让实例可以访问构造函数原型(constructor.prototype)所在原型链上的属性
+ * ③ 如果构造函数返回的结果不是引用数据类型
  */
-// 让实例可以访问到私有属性
-// 让实例可以访问构造函数原型(constructor.prototype)所在原型链上的属性
-// 如果构造函数返回的结果不是引用数据类型
+
 
 function newFactory(ctor, ...args) {
   if (typeof ctor !== 'function') {
--- 数组/数组扁平化.js
+++ 数组/数组扁平化.js
@@ -1,6 +1,37 @@
 /**
  * @description 数组扁平化
  */
-let ary = [1, [2, [3, [4, 5]]], 6];// -> [1, 2, 3, 4, 5, 6]
-let str = JSON.stringify(ary);
+let ary = [1, [2, [3, [4, 5]]], 6] // -> [1, 2, 3, 4, 5, 6]
+let str = JSON.stringify(ary)
 
+// 1. ES6的flat方法
+arr.flat(Infinity)
+
+// 2. 普通递归
+let result = []
+let fn = function (ary) {
+  for (let i = 0; i < ary.length; i++) {
+    let item = ary[i]
+    if (Array.isArray(ary[i])) {
+      fn(item)
+    } else {
+      result.push(item)
+    }
+  }
+}
+
+// 3. reduce函数迭代
+function flatten(ary) {
+  return ary.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? flatten(cur) : cur)
+  }, [])
+}
+
+// 4. 拓展运算符
+//只要有一个元素有数组，那么循环继续
+while (ary.some(Array.isArray())) {
+  ary = [].concat(...ary);
+}
+
+// 5. replace + split
+str.replace(/(\[|\])/g, '').split(',')
